2ecba3f8ba582025f0577869e9995ca5d06cd930
Mukhiddin06 <muhiddinbegiboyev@gmail.com>
2024-06-08 14:00:03 +0500

    Added new files

--- src/pages/cars/cars.jsx
+++ src/pages/cars/cars.jsx
@@ -2,26 +2,39 @@ import "./cars.css"
 import UserModal from "../modal/modal"
 import { useState } from "react"
 import { NavLink } from "react-router-dom";
+import { nanoid } from "nanoid";
 
 const Cars = () => {
-    const [cars, setCars] = useState([]);
+    const [search, setSearch] = useState("")
+    const [cars, setCars] = useState([
+        {id:nanoid(), name:"BMW", price:150000, color:"blue", year:"2024-06-06", brand:"m8"}
+    ]);
+    const [car, setCar] = useState({})
     const [modal, setModal] = useState(false)
 
-    const openModal = () => {
+    const openModal = (item) => {
+        setCar(item)
         setModal(true)
-    }
 
+    }
+    const toggle =()=>{
+        setModal(false)
+        setCar({})
+    }
+    const deleteCar =(id)=>{
+        setCars(cars.filter((car) => car.id !== id));
+    }
     return (
         <>
-            <UserModal open={modal} toggle={() => setModal(false)} cars={cars} setCars={setCars} />
+            <UserModal open={modal} car={car} toggle={toggle} cars={cars} setCars={setCars} />
             <div className="container">
                 <div className="cars-wrapper">
                     <div className="cars">
                         <div className="btn-wrp">
-                            <button className="btn-cars" onClick={openModal}>Add user</button>
+                            <button className="btn-cars" onClick={()=>setModal(true)}>Add user</button>
                         </div>
                         <div className="inp-wrp">
-                            <input type="text" placeholder="Search..." className="inp-cars" />
+                            <input type="text" placeholder="Search..." onChange={(e)=>setSearch(e.target.value)} className="inp-cars" />
                         </div>
                     </div>
                     <div className="cars-table">
@@ -39,13 +52,13 @@ const Cars = () => {
                             </thead>
                             <tbody>
                                 {cars?.filter((item) => {
-              let name = item?.name?.toLowerCase()
-              let find = search.toLowerCase()
-              if(name.includes(find)){
-                return item
-              }
-            }).map((item, index) => (
-                                    <tr key={index}>
+                                    let name = item?.name?.toLowerCase()
+                                    let find = search.toLowerCase()
+                                        if(name.includes(find)){
+                                            return item
+                                        }
+                                    }).map((item, index) => (
+                                    <tr key={item.id}>
                                         <td>{index + 1}</td>
                                         <td>{item.name}</td>
                                         <td>{item.price}</td>
@@ -54,8 +67,8 @@ const Cars = () => {
                                         <td>{item.brand}</td>
                                         <td>
                                             <div className="btn-wrapper">
-                                                <button className="btn-edit"><i className="fa-solid fa-pen-to-square"></i></button>
-                                                <button className="btn-trash"><i className="fa-solid fa-trash-can"></i></button>
+                                                <button onClick={()=>openModal(item)} className="btn-edit"><i className="fa-solid fa-pen-to-square"></i></button>
+                                                <button onClick={()=>deleteCar(item.id)} className="btn-trash"><i className="fa-solid fa-trash-can"></i></button>
                                                 <NavLink to="/single-cars">
                                                     <span><i className="fa-solid fa-eye"></i></span>
                                                 </NavLink>
--- src/pages/modal/modal.jsx
+++ src/pages/modal/modal.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import { Modal, ModalBody, ModalFooter, ModalHeader } from "reactstrap";
 import "./modal.css";
+import { nanoid } from "nanoid";
 
-const UserModal = ({ open, toggle, cars, setCars }) => {
+const UserModal = (props) => {
     const [form, setForm] = useState({});
-
+    const  {open, cars, setCars, toggle, car} = props
     const handleChange = (e) => {
         const { name, value } = e.target;
         setForm({ ...form, [name]: value });
@@ -12,7 +13,23 @@ const UserModal = ({ open, toggle, cars, setCars }) => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        setCars([...cars, form]);
+        if(car.id){
+            let new_cars = cars.map((item)=>{
+                if(item.id === car.id){
+                    item.name = form.name ? form.name : item.name
+                    item.price = form.price ? form.price : item.price
+                    item.year = form.year ? form.year : item.year
+                    item.color = form.color ? form.color : item.color
+                    item.brand = form.brand ? form.brand : item.brand
+                }
+                return item
+            })
+            setCars([...new_cars])
+        }else{
+            let id = nanoid() 
+            cars.push({...form, id})
+            setCars([...cars]);
+        }
         toggle();
     };
 
@@ -23,11 +40,11 @@ const UserModal = ({ open, toggle, cars, setCars }) => {
             </ModalHeader>
             <ModalBody>
                 <form className="modal-form" id="submit" onSubmit={handleSubmit}>
-                    <input onChange={handleChange} type="text" placeholder="Name" name="name" className="modal-input" />
-                    <input onChange={handleChange} type="number" placeholder="Price" name="price" className="modal-input" />
-                    <input onChange={handleChange} type="date" placeholder="Year" name="year" className="modal-input" />
-                    <input onChange={handleChange} type="text" placeholder="Color" name="color" className="modal-input" />
-                    <input onChange={handleChange} type="text" placeholder="Brand" name="brand" className="modal-input" />
+                    <input defaultValue={car.name} onChange={handleChange} type="text" placeholder="Name" name="name" className="modal-input" />
+                    <input defaultValue={car.price} onChange={handleChange} type="number" placeholder="Price" name="price" className="modal-input" />
+                    <input defaultValue={car.year} onChange={handleChange} type="date" placeholder="Year" name="year" className="modal-input" />
+                    <input defaultValue={car.color} onChange={handleChange} type="text" placeholder="Color" name="color" className="modal-input" />
+                    <input defaultValue={car.brand} onChange={handleChange} type="text" placeholder="Brand" name="brand" className="modal-input" />
                 </form>
             </ModalBody>
             <ModalFooter>
